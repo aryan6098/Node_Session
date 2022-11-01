826a84ec59faed538fd15f15123ff1876089d850
Aryan <araj60988@gmail.com>
2022-11-01 15:24:44 +0530

    Complted assignment -  enter input value and shows inputted value

--- routes.js
+++ routes.js
@@ -1,58 +1,41 @@
 const fs = require("fs");
 
+const bodyData = [];
 const requestHanlder = (req,res)=>{
     const url = req.url;
     const method = req.method;
     if (url === "/") {
       res.write("<html>");
-      res.write("<head><title>Enter Message</title></head>");
+      res.write("<head><title>Assignment</title></head>");
       res.write(
-        '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">submit</button></form></body>'
+        '<body><form action="/create-users" method="POST"><label>usename</label><input type="text" name="message"><button type="submit">submit</button></form></body>'
       );
+      bodyData.forEach((val)=>{
+        
+        res.write(val)
+        res.write('</br>')
+      })
+      
       res.write("</html>");
       return res.end();
     }
-    if (url === "/message" && method === "POST") {
-  
-      const body = [];
-      req.on('data', (chunk) => {
-          console.log('data', chunk);
-          body.push(chunk);
-      })
-      return req.on('end' ,()=>{
-          const parseBody = Buffer.concat(body).toString();
-          const message = parseBody.split('=')[1];
-          // fs.writeFileSync('message.txt', message)
-          fs.writeFile('message.txt',message, (err)=>{
-            console.log(err)
-            res.statusCode = 302;
-            res.setHeader("Location", "/");
-            return res.end();
-          })
-      })
-    
-      // fs.writeFileSync("message.txt", "DUMMY");
+    if (url === "/create-users" && method === "POST") {
+        const data = [];
+        req.on('data',(chunk)=>{
+            console.log(chunk)
+            data.push(chunk);
+        })
+        req.on('end',()=>{
+            const parseBody = Buffer.concat(data).toString();
+            console.log(parseBody.split('=')[1]);
+            bodyData.push(parseBody.split('=')[1])
+        })
+        res.statusCode = 302;
+        res.setHeader('Location', '/')
+        res.end()
     
     }
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>MY First Page</title></head>");
-    res.write("<body><h1>Hello from my server side: </h1></body>");
-    res.write("</html>")
-    res.end();
 }
-
-//diffrent way to export our module
-
-// module.exports = requestHanlder;
-
-// module.exports = {
-//     handler: requestHanlder,
-//     someText: 'SOME hard Coded text'
-// }
-
-// module.exports.handler = requestHanlder;
-// module.exports.someText = 'SOME hard Coded text'
 
 exports.hanlder = requestHanlder;
 exports.someText = 'SOME hard Coded text';
