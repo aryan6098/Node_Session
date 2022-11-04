0dd438042c71893d6547211aa5503ea1132a0d38
Aryan <araj60988@gmail.com>
2022-11-04 15:55:46 +0530

    middle ware handler

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-const http = require("http");
-
-const routes = require('./routes');
-
+const express =  require('express')
+const bodyParser = require('body-parser')
+const app = express();
 const port = 8080;
-console.log(routes.someText)
-const server = http.createServer(routes.hanlder);
 
-server.listen(port);
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Subimt</button></form>')
+})
+app.use("/product" ,(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/')
+})
+app.use('/',(req,res,next)=>{
+    res.send("<h1>Hello from te server side</h1>")
+})
+
+
+app.listen(port);
