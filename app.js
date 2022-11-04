353db11d5e5aa5ee3520762c1eb1778063c37497
Aryan <araj60988@gmail.com>
2022-11-04 16:19:01 +0530

    routes handler

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 const express =  require('express')
+
+const admminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop');
 const bodyParser = require('body-parser')
 const app = express();
 const port = 8080;
 
 app.use(bodyParser.urlencoded({extended:false}))
 
-app.use('/add-product',(req,res,next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Subimt</button></form>')
-})
-app.use("/product" ,(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/')
-})
-app.use('/',(req,res,next)=>{
-    res.send("<h1>Hello from te server side</h1>")
-})
+app.use(admminRoutes);
+app.use(shopRoutes);
 
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page Not Found</h1>')
+})
 
 app.listen(port);
