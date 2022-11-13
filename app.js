ed08a155c70c1d0853ae54689219712d8a23604d
aryan6098 <araj60988@gmail.com>
2022-11-14 01:52:44 +0530

    complete routes practice

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const express =  require('express')
-const path = require('path')
-const admminRoutes = require('./routes/admin')
-const shopRoutes = require('./routes/shop');
-const bodyParser = require('body-parser')
+const express = require("express");
+const path = require("path");
+const admminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const rootDir = require("./utils/path");
+const bodyParser = require("body-parser");
+
 const app = express();
 const port = 8080;
 
-app.use(bodyParser.urlencoded({extended:false}))
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(admminRoutes);
 app.use(shopRoutes);
 
@@ -15,8 +17,8 @@ app.use(shopRoutes);
 //     res.status(404).send('<h1>Page Not Found</h1>')
 // })
 
-app.use((req,res,next)=>{
-    res.status(404).sendFile(path.join(__dirname, 'views' ,'404.html'))
-})
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
+});
 
 app.listen(port);
--- routes/admin.js
+++ routes/admin.js
@@ -2,10 +2,11 @@ const express = require('express');
 const path = require('path')
 const Router = express.Router();
 
+const rootDir = require("../utils/path")
 Router.get('/add-product',(req,res,next)=>{
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
 })
-Router.post("/product" ,(req,res,next)=>{
+Router.post("/add-product" ,(req,res,next)=>{
     console.log(req.body);
     res.redirect('/')
 })
